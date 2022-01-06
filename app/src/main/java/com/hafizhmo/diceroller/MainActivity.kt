09a16b98ae44c297d259ca01ba35dc8068f0c757
hafizhMo <hafizh.hizb@gmail.com>
2022-01-06 21:02:05 +0700

    clear warning marks

--- app/src/main/java/com/hafizhmo/diceroller/MainActivity.kt
+++ app/src/main/java/com/hafizhmo/diceroller/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.hafizhmo.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage: ImageView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +22,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when(randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
+        val drawableResource =
+            when (Random().nextInt(6) + 1) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
         diceImage.setImageResource(drawableResource)
     }
 }
